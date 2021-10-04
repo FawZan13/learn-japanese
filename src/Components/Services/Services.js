7fd603125c6da96912bc5a7eff4ce3e809cf90d1
FawZan13 <fawzanbinmokhtar@gmail.com>
2021-10-04 12:47:20 +0600

    ready

---
+++ src/Components/Services/Services.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { Row } from 'react-bootstrap';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import Show from '../Show/Show';
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch("./services.json")
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+    return (
+        <div>
+            <Header></Header>
+            <h2 className="text-center my-5">Services</h2>
+            <Row xs={1} md={4} className="g-4 mx-auto">
+                {
+                    services.map(service => <Show key={service.key} service={service}></Show>
+                    )}
+            </Row>
+            <Footer></Footer>
+
+        </div>
+    );
+};
+
+export default Services;
