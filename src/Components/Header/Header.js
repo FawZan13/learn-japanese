c7dc1df25efa24f89715c3904320fee0fcdad07b
FawZan13 <fawzanbinmokhtar@gmail.com>
2021-10-04 20:04:46 +0600

    last

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="mb-5">
             <Navbar className="py-4" bg="danger" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/home"><img className="me-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcP2lD4WQdnhULK9LUPEV4LaAvoH9_rU9hjpohjQX4ojKLfyAxraqbz0jfa10JIAJwP9Y&usqp=CAU" height="40px" alt=""></img>Learn Japanese</Navbar.Brand>
+                    <Navbar.Brand href="#"><img className="me-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcP2lD4WQdnhULK9LUPEV4LaAvoH9_rU9hjpohjQX4ojKLfyAxraqbz0jfa10JIAJwP9Y&usqp=CAU" height="40px" alt=""></img>Learn Japanese</Navbar.Brand>
                     <Nav className="mx-auto">
                         <Nav.Link><Link className="mx-4 text-decoration-none text-white" to="/Home">Home</Link></Nav.Link>
                         <Nav.Link><Link className="mx-4 text-decoration-none text-white" to="/Services">Services</Link></Nav.Link>
